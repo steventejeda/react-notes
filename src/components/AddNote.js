4e5861535a169ed37987b6a14297424b0df36419
Steven Tejeda <steventejeda@gmail.com>
2021-06-22 13:42:48 -0400

    Adding a character count feature

--- src/components/AddNote.js
+++ src/components/AddNote.js
@@ -2,9 +2,12 @@ import React, { useState } from 'react'
 
 const AddNote = ({handleAddNote}) => {
     const [noteText, setNoteText] = useState('');
+    const characterLimit = 200;
 
     const handleChange = (e) => {
+        if(characterLimit - e.target.value.length >=0){
         setNoteText(e.target.value)
+        }
     }
 
     const handleSaveClick = () => {
@@ -24,7 +27,7 @@ const AddNote = ({handleAddNote}) => {
           >
           </textarea>
           <div className="note-footer">
-              <small>200 remaining</small>
+              <small>{characterLimit - noteText.length} remaining</small>
               <button className="save" onClick={handleSaveClick}>Save</button>
           </div>
       </div>
